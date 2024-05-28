79f620defedf0cbc88fe196f3ce8ef072134e57e
Ricardo Luna Santos <ricardo.luna@utxicotepec.edu.mx>
2024-05-28 10:57:01 -0600

    Agregar vista registeruser

--- Frontend/front-gimnasio/src/components/Login.vue
+++ Frontend/front-gimnasio/src/components/Login.vue
@@ -1,5 +1,4 @@
 <template>
-    <!-- source:https://codepen.io/owaiswiz/pen/jOPvEPB -->
 <div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
     <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
         <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
@@ -9,7 +8,7 @@
             </div>
             <div class="mt-12 flex flex-col items-center">
                 <h1 class="text-2xl xl:text-3xl font-extrabold">
-                    Sign up
+                    Registrar
                 </h1>
                 <div class="w-full flex-1 mt-8">
                     <div class="flex flex-col items-center">
@@ -32,7 +31,7 @@
                                 </svg>
                             </div>
                             <span class="ml-4">
-                                Sign Up with Google
+                                Registrar con Google
                             </span>
                         </button>
 
@@ -45,7 +44,7 @@
                                 </svg>
                             </div>
                             <span class="ml-4">
-                                Sign Up with GitHub
+                                Registrar con GitHub
                             </span>
                         </button>
                     </div>
@@ -53,17 +52,17 @@
                     <div class="my-12 border-b text-center">
                         <div
                             class="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                            Or sign up with e-mail
+                            O registrar con correo electrónico
                         </div>
                     </div>
 
                     <div class="mx-auto max-w-xs">
                         <input
                             class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                            type="email" placeholder="Email" />
+                            type="email" placeholder="Correo" />
                         <input
                             class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                            type="password" placeholder="Password" />
+                            type="password" placeholder="Contraseña" />
                         <button
                             class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
                             <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
@@ -73,17 +72,17 @@
                                 <path d="M20 8v6M23 11h-6" />
                             </svg>
                             <span class="ml-3">
-                                Sign Up
+                                Registrar
                             </span>
                         </button>
                         <p class="mt-6 text-xs text-gray-600 text-center">
-                            I agree to abide by templatana's
+                            Estoy de acuerdo con BullsGym 
                             <a href="#" class="border-b border-gray-500 border-dotted">
-                                Terms of Service
+                                Terminos de  Servicio
                             </a>
-                            and its
+                            y su
                             <a href="#" class="border-b border-gray-500 border-dotted">
-                                Privacy Policy
+                                Politica de Privacidad
                             </a>
                         </p>
                     </div>
--- Frontend/front-gimnasio/src/router/index.js
+++ Frontend/front-gimnasio/src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/components/Login.vue'
+import RegisterUserView from './../components/RegisterUser.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'login',
       component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterUserView
     }
   ]
 })
