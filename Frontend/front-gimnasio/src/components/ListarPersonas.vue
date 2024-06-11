ad5f82e45944d3738b5f2fb1734862c61148e16f
Ricardo Luna Santos <ricardo.luna@utxicotepec.edu.mx>
2024-06-11 10:56:04 -0600

    cambios en componente listar personas

---
+++ Frontend/front-gimnasio/src/components/ListarPersonas.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Foto</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Primer Apellido</th>
+                    <th scope="col">Segundo Apellido</th>
+                    <th scope="col">Titulo de Cortesia</th>
+                    <th scope="col">Fecha de Nacimiento</th>
+                    <th scope="col">Genero</th>
+                    <th scope="col">Tipo de Sangre</th>
+                    <th scope="col">Eliminar</th>
+                    <th scope="col">Editar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td><img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png" width="50px"/></td>
+                    <td>Norma</td>
+                    <td>Garcia</td>
+                    <td>Romero</td>
+                    <td>Mtra.</td>
+                    <td>19/06/1987</td>
+                    <td>Mujer</td>
+                    <td>O+</td>
+                    <td><a href="">Eliminar</a></td>
+                    <td><a href="">Editar</a></td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td><img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png" width="50px"/></td>
+                    <td>Daniel</td>
+                    <td>Garrido</td>
+                    <td>Hernandez</td>
+                    <td>Lic.</td>
+                    <td>19/06/1990</td>
+                    <td>Hombre</td>
+                    <td>A+</td>
+                    <td><a href="">Eliminar</a></td>
+                    <td><a href="">Editar</a></td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td><img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png" width="50px"/></td>
+                    <td>Maria Guadalupe</td>
+                    <td>Salas</td>
+                    <td>Cruz</td>
+                    <td>Ing.</td>
+                    <td>29/02/90</td>
+                    <td>Mujer</td>
+                    <td>B+</td>
+                    <td><a href="">Eliminar</a></td>
+                    <td><a href="">Editar</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
