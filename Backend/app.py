6c5a8f612c349d5976019823d526802b3f31b404
Ricardo Luna Santos <143124590+utxj-ricardo-luna@users.noreply.github.com>
2024-06-21 12:43:28 -0600

    agregar funcion eliminar, actualizar y buscar

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from routes.user import user
+from routes.person import person
 
 app=FastAPI()
 app.include_router(user)
+app.include_router(person)
+
 print ("Hola bienvenido a mi backend")
--- Backend/routes/user.py
+++ Backend/routes/user.py
@@ -13,12 +13,12 @@ class model_user(BaseModel):
     created_at:datetime = datetime.now()
     estatus:bool=False
 
-@user.get("/")
+@user.get('/')
 
 def bienvenida():
     return "Bienvenido al sistema de apis"
 
-@user.get("/users")
+@user.get('/users')
 
 def get_usuarios():
     return users
@@ -29,3 +29,31 @@ def save_usuarios(insert_users:model_user):
     users.append(insert_users)
     print (insert_users)
     return "Datos guardados"
+
+@user.post('/user/{user_id}')
+
+def get_usuario(user_id: str):
+    for user in users:
+        if user.id== user_id:
+            return user
+    return "No existe el registro"
+
+@user.delete('/user/{user_id}')
+
+def delete_usuario(user_id: str):
+    for user in users:
+        if user.id == user_id:
+            users.remove(user)
+            return "Registro eliminado correctamente"
+    return "Registro no encontrado"
+
+@user.put('/user/{user_id}')
+
+def update_usuario(user_id: str, updateUser: model_user):
+    for user in users:
+        if user.id == user_id:
+            user.usuario=updateUser.usuario
+            user.contrasena=updateUser.contrasena
+            user.estatus=updateUser.estatus
+            return "Registro actualizado correctamente"
+    return "Registro no encontrado"
